36ded8d45ed701adec9a88e2cf2283fdd23c6fb6
Princeduke223 <denischege223@gmail.com>
2023-02-16 15:32:13 -0800

    manyundo

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+* main - Entry point
+*
+* This function is the entry point of the program.
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
