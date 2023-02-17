f51e0819b80abc0772fb2bfba0420f06ee7e419a
Princeduke223 <denischege223@gmail.com>
2023-02-16 17:33:53 -0800

    jhhjhhjj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This function is the entry point of the program.
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
