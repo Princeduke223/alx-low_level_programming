e5ab26b9e756b3a1a31ec815c7548517788c5c29
Princeduke223 <denischege223@gmail.com>
2023-02-16 17:22:41 -0800

    jhhjhhjj

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,9 @@ int main(void)
 	int a = 1;
 	char c = "w";
 	printf("Size of char: %d byte(s)\n", sizeof(char));
-
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
-
 	printf("Size of long int: %d byte(s)\n", sizeof(long));
-
 	printf("Size of long long int: %d byte(s)\n", sizeof(long long));
-
 	printf("Size of float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
