b9e398f9d3ee26f28f383eb372c6bc73b9e65bb8
Princeduke223 <denischege223@gmail.com>
2023-02-20 10:24:45 -0800

    question 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <time.h>
 
-/* 
+/*
  * This if statement checks whether the value of x is greater than 10.
 If x is greater than 10, it sets the value of y to 1. Otherwise, it
-sets the value of y to 0. 
+sets the value of y to 0.
 */
 
 int main(void)
