b930e96ee8c1d9ba2e984dd019872a1111fbc81e
Princeduke223 <denischege223@gmail.com>
2023-02-21 13:18:12 -0800

    mwaki

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
-void print_alphabet(void) {
-    char letter = 'a';
-    while (letter <= 'z') {
-        putchar(letter);
-        letter++;
-    }
-    putchar('\n');
+void print_alphabet(void)
+{
+char letter = 'a';
+while (letter <= 'z')
+{
+	putchar(letter);
+	letter++;
+}
+putchar('\n');
 }
