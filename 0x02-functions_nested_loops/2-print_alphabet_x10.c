e0738716b64b3477e6bd9580646acdd7b2f43e37
Princeduke223 <denischege223@gmail.com>
2023-02-22 05:36:09 -0800

    kddkdkd

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,13 +4,14 @@
 void print_alphabet_x10(void)
 {
 	int n;
+
 	for (n = 0; n < 10; n++)
 	{
-		char let = 'a';
-		while (let <= 'z')
+		char c;
+
+		for (c = 'a'; c <= 'z'; c++)
 		{
-			putchar('a');
-			let++;
+			putchar(c);
 		}
 		putchar('\n');
 	}
