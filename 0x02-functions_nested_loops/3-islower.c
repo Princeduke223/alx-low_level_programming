a6f00cf3663ad879e2bdc8e352489bbad1a01bd5
Princeduke223 <denischege223@gmail.com>
2023-02-22 12:49:00 -0800

    hfjfkd

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    r = _islower('H');
+    putchar(r + '0');
+    r = _islower('o');
+    putchar(r + '0');
+    r = _islower(108);
+    putchar(r + '0');
+    putchar('\n');
+    return (0);
+}
