018faa70c5808ffc41a217fc176c71dbfdf55f62
Princeduke223 <denischege223@gmail.com>
2023-02-23 04:37:17 -0800

    i am fire

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/*
+/**
  * _isdigit - check if character is a digit
  * @c:the character t be checked
  * Return: 1 for a character that is a digit or zero for any else
