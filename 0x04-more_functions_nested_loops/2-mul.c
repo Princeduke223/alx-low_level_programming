61e9f510d83708d9196164179416849a74c0bc72
Princeduke223 <denischege223@gmail.com>
2023-02-23 04:52:15 -0800

    mimi ni moto

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * mul - multiplies two numbers
+ * @a:  is the first number
+ * @b: is the second number
+ * Return - c if successfull
+ */
+
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+int mul(int a, int b);
 int _isdigit(int c);
 int _putchar(char c);
 int _isupper(int c);
