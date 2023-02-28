8a6c565f9c23d87eeb7727430dec159f2ca783c4
Princeduke223 <denischege223@gmail.com>
2023-02-28 09:42:04 -0800

    number 2

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -5,14 +5,14 @@
  * swap_int - swips *a with *b
  * @a: variable of *a
  * @b: variable of *b
- * @temp: variable for swaping *a and *b
+ * @i: variable for swaping *a and *b
  * return:swiped numbers
  */
 void swap_int(int *a, int *b)
 {
-	int temp;
+	int i;
 
-	temp = *a;
+	i = *a;
 	*a = *b;
-	*b = temp;
+	*b = i;
 }
