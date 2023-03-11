74f85a6ab33f714b54961775a01f0327c62af979
Princeduke223 <denischege223@gmail.com>
2023-03-11 13:52:33 -0800

    code maniac

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints all arguments
+  * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	  int i;
+    for (i = 0; i < argc; i++) {
+        printf("%s\n", argv[i]);
+    }
+    return (0);
+}
