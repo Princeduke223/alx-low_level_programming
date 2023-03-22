da2e4a44ec6699a0a34ceeb2b1d91cb3c1bbf32d
Princeduke223 <denischege223@gmail.com>
2023-03-22 15:39:32 -0700

    hhhhhhhhhhhh

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,12 +9,12 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-0-print_name.c if (array == NULL || action == NULL)
-0-print_name.c 0-print_name.c return;
+if (array == NULL || action == NULL)
+return;
 
-0-print_name.c while (size-- > 0)
-0-print_name.c {
-0-print_name.c 0-print_name.c action(*array);
-0-print_name.c 0-print_name.c array++;
-0-print_name.c }
+while (size-- > 0)
+{
+action(*array);
+array++;
+}
 }
