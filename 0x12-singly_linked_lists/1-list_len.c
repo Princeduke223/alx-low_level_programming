5baac25135a2d29857dac15d1a29ba56ff927337
Princeduke223 <denischege223@gmail.com>
2023-03-30 17:17:03 -0700

    hhhhhjj

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -10,13 +10,13 @@
  */
 size_t list_len(const list_t *h)
 {
-0-print_list.c size_t elements = 0;
+	size_t elements = 0;
 
-0-print_list.c while (h)
-0-print_list.c {
-0-print_list.c 0-print_list.c elements++;
-0-print_list.c 0-print_list.c h = h->next;
-0-print_list.c }
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
 
-0-print_list.c return (elements);
+	return (elements);
 }
